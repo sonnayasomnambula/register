bb409d5c1607cebb71a6bcf5039b7049abc7c397
glazomitsky <s.glazomitsky@mcsplus.ru>
2021-01-11 11:18:38 +0300

    add application icon

--- app/app.pro
+++ app/app.pro
@@ -20,3 +20,6 @@ FORMS += \
 
 INCLUDEPATH += \
     src
+
+RESOURCES += \
+    resources.qrc
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/app.ico"));
+
     MainWindow w;
     w.show();
     return a.exec();
